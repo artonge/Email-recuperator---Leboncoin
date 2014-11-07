4ec1753f3b92ed3bec75eea5c967d5438fd1d4a9
Louis <artonge.c@gmail.com>
2014-11-07 09:39:56 +0100

    Create recuperator.py

---
+++ recuperator.py
@@ -0,0 +1,46 @@
+import sys
+import urllib2, httplib, re, urllib, os
+
+def fetch(id):
+    # URL
+    url = "http://www.leboncoin.fr/emploi/"+str(id)+".htm"
+    
+    # REQUETE
+
+    result = urllib2.Request(url)
+    
+    try:
+        f = urllib2.urlopen(result)
+    except urllib2.HTTPError, err:
+        return
+
+    data = f.read()
+
+    # REGEX - EMAIL
+    reg = "[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]{2,5}"
+    comReg = re.compile(reg)
+    result = comReg.findall( data.decode('utf-8', 'ignore') )
+    # PRINT RESULT
+    for find in result :
+        f = open('emails.txt', 'a+')
+        f.write(find)
+        f.write('\n')
+        for line in f:
+            print 'lo'
+        f.close
+
+        f = open('number.txt', 'w+')
+        f.write(str(id))
+        for line in f:
+            print 'lo'
+        f.close
+    #fin for
+#fin fetch
+     
+id = 706596642
+i  = 706293442
+
+while i > 0 :
+    fetch(i)
+    i = i-1
+#fin while
